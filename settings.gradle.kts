01c697b730516e038492144d093c25143bd41a57
Michael Kutz <mail@michael-kutz.de>
2022-12-09 13:28:53 +0100

    Try Kotlin version in gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ rootProject.name = "github-actions-experiments"
 include("lib")
 
 pluginManagement {
-    val kotlinVersion = "1.7.22"
+    val kotlinVersion: String by settings
     plugins {
         kotlin("jvm") version "${kotlinVersion}"
         kotlin("plugin.noarg") version "${kotlinVersion}"
